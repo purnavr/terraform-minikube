e00b6364fb4e12c71099d10e8c12178cdec77b85
purna <purnaraomba@gmail.com>
2024-07-30 22:33:18 +0530

    Roboshop Update

--- scripts/init-aws-minikube.sh
+++ scripts/init-aws-minikube.sh
@@ -55,6 +55,10 @@ EOF
 # Apply sysctl params without reboot
 sysctl --system
 
+cd /etc/yum.repos.d/
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
 yum install -y yum-utils curl gettext device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y containerd.io
